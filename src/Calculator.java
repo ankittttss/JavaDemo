97b80913f7aa0846a20ceb73799423497e224c44
ankittttss <ankitsaini955831@gmail.com>
2025-02-22 17:31:46 +0530

    Java - Class/Object Theory

---
+++ src/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+
+    Calculator(){
+
+    }
+
+    public int add(int num1,int num2){
+       return num1 + num2;
+    }
+
+    public int Subtract(int num1,int num2){
+        return num2 - num1;
+    }
+
+    public int Multiplication(int num1,int num2){
+        return num1 * num2;
+    }
+
+    public int Divide(int num1,int num2){
+        return num2/num1;
+    }
+
+    public int Modulo(int num1,int num2){
+        return num2%num1;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(3);
+        System.out.print(60);
+        System.out.print(false);
 
+
+        Calculator c = new Calculator();
+        System.out.println(c.add(10,20));
+        System.out.println(c.Subtract(10,20));
+        System.out.println(c.Divide(10,20));
+
+        int no = 10;
+        float gh = 10.5F;
+        String h = "Ankit";
+
+        System.out.println(no);
+        System.out.println(gh);
+        System.out.println(h);
+
+        // Type Conversion//
+        int g = (int)gh;
+
+        // Arithmetic Operators
+
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println(num1 + num2);
+
+        int num3 = 70;
+        int num4 = 80;
+
+        System.out.println(num4 - num3);
+
+        System.out.println(num1++);
+        System.out.println(num1);
+
+        System.out.println(7<5);
     }
 }
